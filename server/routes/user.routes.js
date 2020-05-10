7d02dcb9b3fceea347cf965e910f825da99d14cf
Shaheer Shahzad <shaheer19962012@gmail.com>
2020-05-10 20:48:18 +0200

    Minor change in user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/user.controller");
 router.get("/", userController.getUsers);
 router.post("/", userController.createUser);
 router.get("/:id", userController.getUser);
-router.put("/:id", userController.editUser);
+router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
