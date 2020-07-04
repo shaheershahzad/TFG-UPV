cff8167d081b1c68feb2fa61aa01265b83ba56c5
Shaheer Shahzad <shaheer19962012@gmail.com>
2020-07-04 03:20:29 +0200

    Project form in modal

--- frontend/src/app/pages/Backoffice/project-management/project-management.component.ts
+++ frontend/src/app/pages/Backoffice/project-management/project-management.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../../services/project.service';
 import { Project } from '../../../models/project';
 
+declare const M: any;
+
 @Component({
   selector: 'app-project-management',
   templateUrl: './project-management.component.html',
@@ -13,6 +15,12 @@ export class ProjectManagementComponent implements OnInit {
   constructor(public projectService: ProjectService) { }
 
   ngOnInit(): void {
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems);
+    });
+
     this.getProjects();
   }
 
@@ -23,4 +31,41 @@ export class ProjectManagementComponent implements OnInit {
     });
   }
 
+  addProject(form): void{
+    console.log(form.value);
+    this.projectService.addProject(form.value).subscribe( res => {
+      this.clearForm(form);
+      M.toast({html: "Project saved"});
+      this.getProjects();
+      console.log(res);
+    });
+  }
+
+  viewProjectDetails(form, project: Project) {
+    this.projectService.selectedProject = project;
+    //this.setFormValues(form, project);
+    document.getElementById("addProjectAction").click();
+  }
+
+  editProject(project: Project) {
+    this.projectService.selectedProject = project;
+    (<HTMLInputElement>document.querySelector('#addProjectButton')).innerText = "Actualizar";
+    document.getElementById("addProjectAction").click();
+  }
+
+  deleteProject(id: String) {
+    console.log(id);
+  }
+
+  clearForm(form) {
+    form.reset();
+    this.projectService.selectedProject = new Project();
+  }
+
+  setFormValues(form, project: Project){
+    form.setValue({
+      name: project.name,
+      description: project.description
+    });
+  }
 }
