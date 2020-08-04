a3cb47653f820998be566e17d9cea620ddb77736
Shaheer Shahzad <shaheer19962012@gmail.com>
2020-08-04 19:31:05 +0200

    New components routing done

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -11,6 +11,11 @@ import { UserManagementComponent } from './pages/Backoffice/user-management/user
 import { ProjectManagementComponent } from './pages/Backoffice/project-management/project-management.component';
 import { NewsletterManagementComponent } from './pages/Backoffice/newsletter-management/newsletter-management.component';
 import { ProjectDetailsComponent } from './components/project-details/project-details.component';
+import { NewsManagementComponent } from './pages/Backoffice/news-management/news-management.component';
+import { EventManagementComponent } from './pages/Backoffice/event-management/event-management.component';
+import { FileManagementComponent } from './pages/Backoffice/file-management/file-management.component';
+import { WebdataManagementComponent } from './pages/Backoffice/webdata-management/webdata-management.component';
+import { DonationManagementComponent } from './pages/Backoffice/donation-management/donation-management.component';
 
 // Services
 import { AuthService } from '../app/services/auth.service';
@@ -27,6 +32,11 @@ const routes: Routes = [
   { path: 'backoffice/users', component: UserManagementComponent, canActivate : [AuthService] },
   { path: 'backoffice/projects', component: ProjectManagementComponent, canActivate : [AuthService] },
   { path: 'backoffice/newsletter', component: NewsletterManagementComponent, canActivate : [AuthService] },
+  { path: 'backoffice/news', component: NewsManagementComponent, canActivate : [AuthService] },
+  { path: 'backoffice/events', component: EventManagementComponent, canActivate : [AuthService] },
+  { path: 'backoffice/files', component: FileManagementComponent, canActivate : [AuthService] },
+  { path: 'backoffice/webdata', component: WebdataManagementComponent, canActivate : [AuthService] },
+  { path: 'backoffice/donations', component: DonationManagementComponent, canActivate : [AuthService] },
   { path: '**', pathMatch: 'full', redirectTo: 'login' }
 ];
 
