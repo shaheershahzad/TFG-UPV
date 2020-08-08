cea60650ceba77a55f0716bdac8c3d983397ff10
Shaheer Shahzad <shaheer19962012@gmail.com>
2020-08-08 23:38:08 +0200

    News on home done

--- frontend/src/app/pages/news/news.component.ts
+++ frontend/src/app/pages/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { News } from '../../models/news';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  public newsAvailable: boolean = false;
+
+  constructor(public newsService: NewsService) { }
 
   ngOnInit(): void {
+    this.getNews();
+  }
+
+  getNews(){
+    this.newsService.getAllNews().subscribe(res => {
+      this.newsService.news = res as News[];
+
+      if(this.newsService.news.length > 0){
+        this.newsAvailable = true;
+      }else{
+        this.newsAvailable = false;
+      }
+    });
   }
 
 }
